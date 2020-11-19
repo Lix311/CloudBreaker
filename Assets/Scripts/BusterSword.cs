4b6e52d33aabeef5a2b6fe27dea4c848f92010dc
Lix311 <alexsterrantino@gmail.com>
2020-11-19 16:19:16 -0500

    sword movement and mathclamp

---
+++ Assets/Scripts/BusterSword.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusterSword : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 2f;
+    [SerializeField] float maxX = 14f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 swordPos = new Vector2(mousePosInUnits, transform.position.y);
+        swordPos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = swordPos;
+    }
+}

a0ada0b01fcfcc823b6921f98f05666e04a74356
Lix311 <alexsterrantino@gmail.com>
2020-11-22 17:06:33 -0500

    sword sticks to materia

---
+++ Assets/Scripts/Materia.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Materia : MonoBehaviour
+{
+    // params
+    [SerializeField] BusterSword bustersword1;
+
+    // state
+    Vector2 swordToMateriaVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swordToMateriaVector = transform.position - bustersword1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 swordPos = new Vector2(bustersword1.transform.position.x, bustersword1.transform.position.y);
+        transform.position = swordPos + swordToMateriaVector;
+    }
+}

6c050ad88f430a43835e0acb31d31d6dd52cbcd9
Lix311 <alexsterrantino@gmail.com>
2020-11-24 15:36:02 -0500

    launch ball

--- Assets/Scripts/Materia.cs
+++ Assets/Scripts/Materia.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,12 @@ public class Materia : MonoBehaviour
 {
     // params
     [SerializeField] BusterSword bustersword1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     // state
     Vector2 swordToMateriaVector;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,27 @@ public class Materia : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+       
+
+        if(!hasStarted)
+        {
+            LockSwordToMateria();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockSwordToMateria()
     {
         Vector2 swordPos = new Vector2(bustersword1.transform.position.x, bustersword1.transform.position.y);
         transform.position = swordPos + swordToMateriaVector;

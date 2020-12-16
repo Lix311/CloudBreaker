6b3b24682e60c4d4090ec6026fd7ad03c42a2ac4
Lix311 <alexsterrantino@gmail.com>
2020-12-16 16:34:48 -0500

    better maxHits logic

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,7 +8,6 @@ public class Block : MonoBehaviour
     // config params
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
-    [SerializeField] int maxHits;
     [SerializeField] Sprite[] hitSprites;
 
     // cashed reference
@@ -46,6 +45,7 @@ public class Block : MonoBehaviour
     {
         
             timesHit++;
+        int maxHits = hitSprites.Length + 1;
             if (timesHit >= maxHits)
             {
                 DestroyBlock();
@@ -59,7 +59,15 @@ public class Block : MonoBehaviour
     private void ShowNextHitSprite()
     {
         int spriteIndex = timesHit - 1;
-        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite is missing from array" + gameObject.name);
+
+        }
     }
 
     private void DestroyBlock()

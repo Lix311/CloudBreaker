17aa0ee53dbbfedeaf4095ef4ba713bd8bed3ed1
Lix311 <alexsterrantino@gmail.com>
2020-12-18 17:11:30 -0500

    added auto play feature

--- Assets/Scripts/BusterSword.cs
+++ Assets/Scripts/BusterSword.cs
@@ -7,18 +7,34 @@ public class BusterSword : MonoBehaviour
     [SerializeField] float screenWidthInUnits = 23f;
     [SerializeField] float minX = 2f;
     [SerializeField] float maxX = 21f;
+
+    // cached references
+    GameSession theGameSession;
+    Materia theMateria;
     // Start is called before the first frame update
     void Start()
     {
-        
+        theGameSession = FindObjectOfType<GameSession>();
+        theMateria = FindObjectOfType<Materia>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 swordPos = new Vector2(mousePosInUnits, transform.position.y);
-        swordPos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        Vector2 swordPos = new Vector2(transform.position.x, transform.position.y);
+        swordPos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = swordPos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled())
+        {
+            return theMateria.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
